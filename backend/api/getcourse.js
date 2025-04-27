284cd6837ff81148bd3cfc0b5b4df7e7f0fdf31f
Soham Anand Ladgaonkar <95856008+Soham1903@users.noreply.github.com>
2025-04-27 12:46:03 +0530

    Update getcourse.js

--- backend/api/getcourse.js
+++ backend/api/getcourse.js
@@ -1,5 +1,4 @@
-import dbConnection from "../../lib/db.js"; // Connect to MongoDB
-import Course from "../../model/courseSchema.js";
+
 
 export default function handler(req, res) {
   res.status(200).json({ message: "Hello from backend!" });
